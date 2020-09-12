23780dad055693ac012d236a54d2a2d9d5fc9849
Janchorizo <jancho@usal.es>
2020-09-12 18:47:42 +0200

    Adapt top bar to allow toggling languages too.

--- src/components/TopBar/topBar.js
+++ src/components/TopBar/topBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 import style from './style.module.css'
 
 
-export default function TopBar({usingLightTheme, setUsingLightTheme, lang, setLang, setData}) {
+export default function TopBar({usingLightTheme, setUsingLightTheme, usingEspLang, setUsingEspLang, setData}) {
   
   const cssClasses = [
     style.topBar,
@@ -14,6 +14,8 @@ export default function TopBar({usingLightTheme, setUsingLightTheme, lang, setLa
 
   const lightIcon = usingLightTheme === true ? 'on' : 'off';
   const darkIcon = usingLightTheme === false ? 'on' : 'off';
+  const espIcon = usingEspLang === true ? 'on' : 'off';
+  const engIcon = usingEspLang === false ? 'on' : 'off';
 
   return <div className={cssClasses}>
     <div className="">
@@ -23,10 +25,15 @@ export default function TopBar({usingLightTheme, setUsingLightTheme, lang, setLa
     <div className="two-third">
     </div>
     <div className={style.optionsContainer}>
-      <button className={style.themeToggle} onClick={() => setUsingLightTheme(!usingLightTheme)}>
-        <span className={"mu mu-radio-" + darkIcon} /> dark mode
-        <hr/>
+      <button className={style.toggleButton} onClick={() => setUsingLightTheme(!usingLightTheme)}>
         light mode <span className={"mu mu-radio-" + lightIcon}/>
+        <hr/>
+        <span className={"mu mu-radio-" + darkIcon} /> dark mode
+      </button>
+      <button className={style.toggleButton} onClick={() => setUsingEspLang(!usingEspLang)}>
+        esp <span className={"mu mu-radio-" + espIcon}/>
+        <hr/>
+        <span className={"mu mu-radio-" + engIcon} /> eng
       </button>
     </div>
   </div>
