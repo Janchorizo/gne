6ad78980bc391837e48339c5f825153411db42e7
Janchorizo <jancho@usal.es>
2020-09-13 00:35:04 +0200

    Add upload modal to exported components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as App} from './App';
 export {default as TopBar} from './TopBar';
 export {default as HelpModal} from './HelpModal';
+export {default as UploadModal} from './UploadModal';
