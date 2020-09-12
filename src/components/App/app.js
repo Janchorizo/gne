76b2cb3d103023423bc91d16428f713bb5e7aec2
Janchorizo <jancho@usal.es>
2020-09-12 18:33:24 +0200

    Use newer top bar component.

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,18 +1,24 @@
 import React, {useState} from 'react'
 
+import {TopBar} from 'components'
+
 import style from './style.module.css'
 import lightTheme from 'themes/light.module.css'
 import darkTheme from 'themes/dark.module.css'
 
 
 export default function App(props) {
-  const [useLightTheme, setUseLightTheme] = useState(true)
+  const [usingLightTheme, setUsingLightTheme] = useState(true)
+  const [lang, setLang] = useState(true)
+  const [data, setData] = useState(true)
+
   const cssClasses = [
     style.app,
-    useLightTheme === true ? lightTheme.theme : darkTheme.theme
+    usingLightTheme === true ? lightTheme.theme : darkTheme.theme
   ].join(' ')
   
-  return <div className={cssClasses} onClick={() => setUseLightTheme(!useLightTheme)}>
+  return <div className={cssClasses}>
+    <TopBar {... {usingLightTheme, setUsingLightTheme, lang, setLang, setData}} />
     <h1>GNE Graphviz Network Explorer</h1>
     </div>
 }
