6ed68d82f30edfac2c0eb8a6ddf4470bbf26c5a5
Janchorizo <jancho@usal.es>
2020-09-23 19:22:47 +0200

    Implement a data preview table.

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export {default as TopBar} from './TopBar';
 export {default as HelpModal} from './HelpModal';
 export {default as UploadModal} from './UploadModal';
 export {default as PropertyTable} from './PropertyTable';
+export {default as DataPreview} from './DataPreview';
