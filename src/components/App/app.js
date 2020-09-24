847e8e9b93438f6aaa71a17ce8f656c71e4402bf
Janchorizo <jancho@usal.es>
2020-09-24 19:26:56 +0200

    Remove dummy response load.

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
 
 import {TopBar, DataPreview} from 'components';
-import {processNetwork} from 'common/helpers';
-import expectedResponse from 'common/network.json';
+//import {processNetwork} from 'common/helpers';
+//import expectedResponse from 'common/network.json';
 
 import style from './style.module.css';
 import lightTheme from 'themes/light.module.css';
@@ -17,8 +17,8 @@ export default function App(props) {
   const [usingEspLang, setUsingEspLang] = useState(true);
   const [data, setData] = useState(null);
 
-  useEffect(() => {processNetwork(expectedResponse).then(setData)}, []);
-  console.log(data);
+//  useEffect(() => {processNetwork(expectedResponse).then(setData)}, []);
+//  console.log(data);
 
   const cssClasses = [
     style.app,
