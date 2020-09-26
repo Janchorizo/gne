092a9dc8ce298ebb0c9e542bfa014269f100f4f5
Janchorizo <jancho@usal.es>
2020-09-26 09:20:53 +0200

    Add prop types and js doc to ClipboardCopy component.

--- src/common/components/ClipboardCopy.js
+++ src/common/components/ClipboardCopy.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import {ClipoardUtility} from 'common/helpers';
 import style from './style.module.css';
 
+/**
+ * Clipboard-icon button for copying to the system clipboard.
+ * Additional CSS classes [tooltip-top, tooltip-right, tooltip-left]
+ * can be used to customize tooltip positioning.
+ * @param   {string} msg Tooltip text content.
+ * @param   {string} content The content to be copied upon click.
+ * @param   {string} extraClassName Extra CSS classes to be added.
+ * @return  {React.Component} A React component
+ */
 export default function ClipboardCopy({msg, content, extraClassName=''}) {
   const className = `${style.clipboard} ${extraClassName}`;
   return <span
@@ -10,3 +20,9 @@ export default function ClipboardCopy({msg, content, extraClassName=''}) {
     className={className}
     onClick={() => ClipoardUtility.copy(content)}>📋</span>;
 }
+
+ClipboardCopy.propTypes = {
+  msg: PropTypes.string,
+  content: PropTypes.string,
+  extraClassName: PropTypes.string,
+};
