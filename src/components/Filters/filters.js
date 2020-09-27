36e7752e968d7886d81cd69aa618fb0f13d55139
Janchorizo <jancho@usal.es>
2020-09-27 23:08:22 +0200

    Add effect for resetting filters.

--- src/components/Filters/filters.js
+++ src/components/Filters/filters.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 
 import style from './style.module.css';
@@ -8,14 +8,32 @@ import style from './style.module.css';
  * Filter container.
  * @return {React.Component} A react component.
  */
-export default function Filters({data, setData}) {
-  const [adding, seAdding] = useState(false);
+export default function Filters({fetched, setData}) {
+  const [filteredDimensions, setFilteredDimensions] = useState([]);
+  const [filters, setFilters] = useState({});
+  const [adding, setAdding] = useState(false);
+
+  useEffect(() => {
+    setFilteredDimensions([]);
+    if (fetched !== null) {
+      setFilters(Object.fromEntries(fetched.nodes.map(d => [d.address, []])));
+    }
+    setData(fetched);
+  }, [fetched]);
+
+  const addButton = <button onClick={() => setAdding(true)}>
+    Add filter <span className="mu mu-plus"></span>
+    </button>;
+  const cancelButton = <button onClick={() => setAdding(false)}>
+    Cancel <span className="mu mu-cancel"></span>
+    </button>;
 
   return <div className={style.filters}>
+    {adding === false ? addButton : cancelButton}
   </div>;
 }
 
 Filters.propTypes = {
-  data: PropTypes.object,
+  fetched: PropTypes.object,
   setData: PropTypes.func,
 };
