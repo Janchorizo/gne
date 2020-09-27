3712b3b2336fc712c54caa327f90a82361f971db
Janchorizo <jancho@usal.es>
2020-09-27 22:51:52 +0200

    Create component to hold the filters.

---
+++ src/components/Filters/filters.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+import style from './style.module.css';
+
+
+/**
+ * Filter container.
+ * @return {React.Component} A react component.
+ */
+export default function Filters({data, setData}) {
+  const [adding, seAdding] = useState(false);
+
+  return <div className={style.filters}>
+  </div>;
+}
+
+Filters.propTypes = {
+  data: PropTypes.object,
+  setData: PropTypes.func,
+};
